8beb98267b7802b3c861589151a6ee0a10943ef4
santhosh <santhoshsiddappa1990@gmail.com>
2025-07-23 22:01:19 +0530

    Added entity classes

---
+++ src/main/java/com/db/app/finwin/entity/CustomerGoal.java
@@ -0,0 +1,16 @@
+package com.db.app.finwin.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name="CUSTOMER_GOALS")
+public class CustomerGoal {
+}
---
+++ src/main/java/com/db/app/finwin/entity/CustomerIncome.java
@@ -0,0 +1,18 @@
+package com.db.app.finwin.entity;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name="CUSTOMER_INCOME")
+public class CustomerIncome {
+}
