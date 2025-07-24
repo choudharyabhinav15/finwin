ed0acfb612897799fda88891eea2aff332455f8e
Rajesh Singh <rajesh.navin@gmail.com>
2025-07-24 16:17:24 +0530

    Added entity classes

--- src/main/java/com/db/app/finwin/entity/CustomerGoal.java
+++ src/main/java/com/db/app/finwin/entity/CustomerGoal.java
@@ -1,8 +1,9 @@
 package com.db.app.finwin.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -12,5 +13,27 @@ import lombok.*;
 @ToString
 @Entity
 @Table(name="CUSTOMER_GOALS")
+
+
 public class CustomerGoal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="ID")
+    Long id;
+
+
+    @Column(name="GOAL_DUE_DATE")
+    Date dueDate;
+
+    @Column(name="GOAL_AMOUNT")
+    Double goalAmount;
+
+    @Column(name="CURRENCY")
+    String currency;
+
+
+    @ManyToMany
+    @JoinColumn(name = "CUSTOMER_ID",referencedColumnName = "ID",nullable = false,updatable = false)
+    private Customer customer;
 }
